6aaf8d591bf6c61f881d694995a67cfd8858a897
Mattew-Kentor <popok3580@gmail.com>
2023-04-12 22:20:47 +0300

    OrderModel

--- stylehub/market/models.py
+++ stylehub/market/models.py
@@ -1,6 +1,10 @@
 """Market models"""
+from datetime import datetime
+from typing import Any, Union
+
 from django.db import models
 
+import auth.models
 import core.models
 
 
@@ -81,3 +85,123 @@ class CategoryBase(core.models.BaseCreature):
             core.models.BaseCreature.created.field.name,
             core.models.BaseCreature.name.field.name,
         )
+
+
+class OrderCustom(models.Model):
+    """
+    order model
+    describes user`s order
+
+    user: id FK -> auth.User
+    max_price: integer. User`s budget
+    header: char[100]. Short essence of order
+    text: Task for designer
+    status: char[4]. Choices
+    created: datetime. Creation datetime.
+    edited: datetime. Editing datetime.
+    designer: id FK -> auth.User.
+    """
+
+    process_choices = (
+        ('wait', 'в обработке'),
+        ('got', 'принят'),
+        ('proc', 'в процессе'),
+        ('deli', 'в доставке'),
+        ('done', 'выполнен'),
+    )
+
+    user: Union[
+        'auth.models.User', 'models.ForeignKey[Any, Any]'
+    ] = models.ForeignKey(
+        auth.models.User,
+        verbose_name='заказчик',
+        help_text='Пользователь, оформивший заказ',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=False,
+    )
+    max_price: Union[
+        int, 'models.IntegerField[Any, Any]'
+    ] = models.IntegerField(
+        verbose_name='максимальная сумма заказа',
+        help_text='Бюджет пользователя',
+    )
+    header: Union[str, 'models.CharField[Any, Any]'] = models.CharField(
+        verbose_name='заголовок',
+        help_text='Заголовок заказа',
+        max_length=100,
+        blank=False,
+        null=False,
+    )
+    text: Union[str, 'models.TextField[Any, Any]'] = models.TextField(
+        verbose_name='описание стиля',
+        help_text='Опишите стиль, добавьте интересные факты',
+        blank=True,
+        null=True,
+    )
+    status: Union[str, 'models.CharField[Any, Any]'] = models.CharField(
+        verbose_name='заголовок',
+        help_text='Заголовок заказа',
+        default=process_choices[0][0],
+        choices=process_choices,
+        max_length=4,
+        blank=False,
+        null=False,
+    )
+    designer: Union[
+        'auth.models.User',
+        'models.ForeignKey[Any, Any]'
+    ] = models.ForeignKey(
+        auth.models.User,
+        verbose_name='дизайнер',
+        help_text='Дизайнер, получивший заказ',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=False,
+    )
+    created: Union[
+        datetime, 'models.DateTimeField[Any, Any]'
+    ] = models.DateTimeField(
+        verbose_name='дата и время создания',
+        help_text='Автоматически выставляется при создании',
+        auto_now_add=True,
+        blank=False,
+        null=False,
+    )
+    edited: Union[
+        datetime,
+        'models.DateTimeField[Any, Any]',
+    ] = models.DateTimeField(
+        verbose_name='дата и время последнего редактирования',
+        help_text='Автоматически выставляется при изменении объекта',
+        auto_now=True,
+        blank=False,
+        null=False,
+    )
+
+
+class OrderPicture(models.Model):
+    """
+    order model
+    describes user`s order
+
+    picture: image.
+    order: id FK -> OrderCustom
+    """
+
+    picture: Union[
+        'models.ImageField[Any, Any]',
+    ] = models.ImageField(
+        verbose_name='изображение',
+        help_text='Изображение желаемого дизайна',
+        upload_to='/media/uploads/order_pictures',
+    )
+    order: Union[
+        'OrderCustom',
+        'models.ForeignKey[Any, Any]'
+    ] = models.ForeignKey(
+        OrderCustom,
+        verbose_name='заказ',
+        help_text='Номер заказа',
+        on_delete=models.SET_NULL,
+    )
