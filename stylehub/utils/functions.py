555de590e197839edbbc97fb5169be4109ef02b3
G1ebS <glev.shooter06@gmail.com>
2023-04-13 17:25:05 +0300

    deleted comments

--- stylehub/utils/functions.py
+++ stylehub/utils/functions.py
@@ -4,30 +4,6 @@ from uuid import uuid4
 
 from django.conf import settings
 
-# path_mapping = {
-#     'Item': 'uploads/items/main_images',
-#     'ItemPicture': 'uploads/items/items_gallery',
-#     'Messages': 'uploads/chat/messages',
-# }
-
-
-# def get_upload_location(instance: Any, filename: str) -> str:
-#     """
-#     Returns directory to upload file from
-#     some model file field
-
-#     all paths that mapping files is in path_mapping
-#     'ModelClassName': 'path/from/media'
-#     """
-#     global path_mapping
-#     try:
-#         path = path_mapping[instance.__class__.__name__]
-#     except KeyError:
-#         path = 'Unknown'
-
-#     name = str(uuid4()) + f'.{filename.split(".")[-1]}'
-#     return str(settings.MEDIA_ROOT / path / name)
-
 
 def get_item_images_upload_location(instance: Any, filename: str) -> str:
     """
