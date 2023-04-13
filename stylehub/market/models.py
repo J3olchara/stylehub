949d53675e0069695750ebec76d054bfc5d10cfd
Arseniy <senya.borodulin@mail.ru>
2023-04-13 17:44:16 +0300

    task: added model cart and ended User model

--- stylehub/market/models.py
+++ stylehub/market/models.py
@@ -201,6 +201,7 @@ class Cart(core.models.CreatedEdited):
     edited: datetime. editing datetime
     items: QuerySet[market.models.Item]. chosen items
     """
+
     items = models.ManyToManyField(
         verbose_name='предметы в корзине',
         help_text='Предметы, которые пользователь добавил в корзину',
