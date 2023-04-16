012273c628aaed75189139b54598eab599e337a1
G1ebS <glev.shooter06@gmail.com>
2023-04-16 23:15:49 +0300

    Fix issues 2

--- stylehub/clothes/tests/testmanagers.py
+++ stylehub/clothes/tests/testmanagers.py
@@ -1,5 +1,6 @@
 """ test clothes app managers """
 
+import auth.models
 import clothes.models
 from market.tests.base import MarketSetUp
 
@@ -9,17 +10,34 @@ class TestOrdersManager(MarketSetUp):
 
     def test_get_user_orders(self):
         """test order manager test_get_user_orders method"""
-        self.order = clothes.models.OrderClothes.objects.create(
-            user=self.user,
-            designer=self.designer_user,
-            item=self.item1,
-            sum=1200,
+        self.order_user_password = 'sghhgfdsdfsdfasdhgfdfsda'
+        self.order_designer_user_password = 'rfghjfsdfsbvncxvcxzvcbnvnbvcvbx'
+
+        self.order_user = auth.models.User.objects.create_user(
+            username='test_order_cloth_user',
+            email='test_order_cloth_user@gmail.com',
+            password=self.order_user_password,
         )
-        self.order1 = clothes.models.OrderClothes.objects.create(
-            user=self.user,
-            designer=self.designer_user,
+        self.order_designer_user = auth.models.User.objects.create_user(
+            username='test_order_cloth_designer_user',
+            email='test_order_cloth_designer_user@gmail.com',
+            password=self.order_designer_user_password,
+        )
+        self.order_designer = self.order_designer_user.make_designer()
+
+        self.order = clothes.models.OrderClothes.objects.create(
+            user=self.order_user,
+            designer=self.order_designer_user,
+            item=self.item1,
+            sum=self.item1.cost,
+        )
+        self.order = clothes.models.OrderClothes.objects.create(
+            user=self.order_user,
+            designer=self.order_designer_user,
             item=self.item2,
             sum=self.item2.cost,
         )
-        orders = clothes.models.OrderClothes.objects.get_user_orders(self.user)
+        orders = clothes.models.OrderClothes.objects.get_user_orders(
+            self.order_user
+        )
         self.assertEqual(len(orders), 2)
